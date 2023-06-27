3f1226f57e383019c35c94b69657b82e8a13e5a3
Kevin Le <iamkevinvle@gmail.com>
2023-06-27 10:58:16 -0700

    define movie model/entity

---
+++ src/main/java/movielist/controllers/MovieListController.java
@@ -0,0 +1,23 @@
+package movielist.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieListController {
+  // CHORE: GET ALL MOVIES
+  @GetMapping("/")
+
+  // CHORE: GET 1 MOVIE BY ID
+
+  // CHORE: ADD A MOVIE TO LIST
+
+  // CHORE: TOGGLE MOVIE SEEN
+
+  // CHORE: DELETE MOVIE FROM LIST
+
+
+
+}
---
+++ src/main/java/movielist/entities/Movie.java
@@ -0,0 +1,27 @@
+package movielist.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Movie {
+  // movies have id, names, watched
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  private Boolean watched;
+
+
+
+
+}
