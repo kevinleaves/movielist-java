7820e511ba9e559e1dc24cd6394f591ff491c6d0
Kevin Le <iamkevinvle@gmail.com>
2023-06-27 13:54:07 -0700

    autowired annotation to instantiate repo

--- src/main/java/movielist/controllers/MovieListController.java
+++ src/main/java/movielist/controllers/MovieListController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -19,18 +19,27 @@ import movielist.repositories.MovieRepository;
 @RestController
 @RequestMapping("/movies")
 public class MovieListController {
-  // INJECT REPOSITORY
+  /*
+  INJECT REPOSITORY w/ @Autowired annotation on controller instance field:
+  instantiates movieRepository without need for constructor method.
+  */
+
+  @Autowired
   private MovieRepository movieRepository;
 
-  // need a constructor method to instantiate the repository
-  public MovieListController(MovieRepository movieRepository) {
+  /*
+   * INJECT REPOSITORY w/o @Autowired
+   * need a constructor method to instantiate the repository
+   *
+   public MovieListController(MovieRepository movieRepository) {
     this.movieRepository = movieRepository;
   }
+   */
 
   // CHORE: GET ALL MOVIES
   @GetMapping
   public Iterable<Movie> getAllMovies() {
-    return this.movieRepository.findAll();
+    return movieRepository.findAll();
   }
 
   // CHORE: ADD A MOVIE TO LIST
@@ -41,7 +50,7 @@ public class MovieListController {
     Movie newMovie = new Movie();
     newMovie.setName(name);
     newMovie.setWatched(watched);
-    this.movieRepository.save(newMovie);
+    movieRepository.save(newMovie);
     return newMovie;
   }
 
@@ -50,7 +59,7 @@ public class MovieListController {
   public Movie getMovieById(@PathVariable("id") Long id) {
 
     // check optional
-    Optional<Movie> optionalMovie = this.movieRepository.findById(id);
+    Optional<Movie> optionalMovie = movieRepository.findById(id);
     if (!optionalMovie.isPresent()) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found with ID: " + id);
     }
@@ -59,6 +68,7 @@ public class MovieListController {
     return optionalMovie.get();
   }
   // CHORE: TOGGLE MOVIE SEEN
+
 
   // CHORE: DELETE MOVIE FROM LIST
 
